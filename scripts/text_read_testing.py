d16b0f29767fe279d2b0aa57f6a991c38c1151ae
arche-knight532 <archeknight532@gmail.com>
2023-01-19 16:09:11 -0700

    add logging functionality

--- scripts/text_read_testing.py
+++ scripts/text_read_testing.py
@@ -15,6 +15,7 @@ from configparser import ConfigParser
 # - If logging is set to "enabled", log data on pulls into a dict object, load into a file at end of run
 
 def loadConfig():
+    #loads config file "config.ini"
     global x1, x2, x3, x4, y1, y2, y3, y4, arts, description, pullLimit, logging
     print("loading config")
     config = ConfigParser()
@@ -42,18 +43,17 @@ def loadConfig():
 
 
 def takeScreenshot():
-    #code to take screenshot, save to screenshot.png in current directory
+    #takes screenshot, save to screenshot.png in current directory
     print("taking screenshot")
     image_name = "screenshot.png"
     screenshot = ImageGrab.grab()
     filepath = f".\\{image_name}"
     screenshot.save(filepath)
-    print("screenshot saved")
-    return filepath
+    #print("screenshot saved")
 
 
 def cropImage():
-    #code for cropping out the art name and description of the art
+    #crops out the art name and description of the art, saves to corresponding files
     print("cropping image")
     image_name = "screenshot.png"
     img = Image.open(image_name)
@@ -62,11 +62,12 @@ def cropImage():
     #imgCropped.show()
     imgCroppedArt.save("artName.png")
     imgCroppedDescription.save("artDescription.png")
-    print("cropped image saved")
+    #print("cropped image saved")
 
 
 def checkArtName():
-    #Code for checking the name of the art
+    #check the name of the art
+    #TODO: change tesseract location to a config item?
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
     artPulled = pytesseract.image_to_string(Image.open('artName.png')).strip()
     print(f"Art pulled: '{artPulled}'")
@@ -74,7 +75,8 @@ def checkArtName():
 
 
 def checkArtDescription():
-    #Code for checking if an art is descriptionless
+    #check if an art is descriptionless
+    #TODO: change tesseract location to a config item?
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
     artDescription = pytesseract.image_to_string(Image.open('artDescription.png')).strip()
     print(f"Art description: '{artDescription}'")
@@ -82,40 +84,69 @@ def checkArtDescription():
 
 
 def countArtPulls():
-    #Code for counting art pulls goes here
-    pass
+    #count arts pulled for logging if enabled
+    global artsPulledLog, artsPulledCounts, artName, description
+
+    #handle prepending 'd.' for descriptionless arts
+    if artName != "" and description == "":
+        artName = f"d.{artName}"
+    #log art into arrays
+    if artName not in artsPulledLog:
+        artsPulledLog.append(artName)
+        artsPulledCounts.append(1)
+    else:
+        index = artsPulledLog.index(artName)
+        artsPulledCounts[index] = artsPulledCounts[index] + 1
 
 
 def writeLogFile():
-    #Code for output log file on art pulls goes here
-    pass
+    #write counts to log if logging is enabled
+    global artsPulledLog, artsPulledCounts, artName, description
+
+    outputFilename = "artsLog.csv";
+    #TODO: potentially sort arrays so that arts are in order of pull count?
+    file = open(outputFilename, "w")
+    index = 0
+    length = len(artsPulledLog)
+    while index < length:
+        #print(f"art: '{artsPulledLog[index]}', count: '{artsPulledCounts[index]}'")
+        file.write(f"{artsPulledLog[index]},{artsPulledCounts[index]}\n")
+        index = index + 1
+    file.close()
+    print(f"Written to output file {outputFilename}")
 
 
 def main():
-    #artsLog to be dict data type, only used if logging == "enabled"
-    global artsLog, artName, description
-    iteration = 1
+    #artsPulledLog and artsPulledCounts to be used for tracking pulls if logging == "enabled"
+    global artsPulledLog, artsPulledCounts, artName, description
+    
+    #initialize values
     loadConfig()
+    artsPulledLog = []
+    artsPulledCounts = []
+    iteration = 1
+
+    #loop for pullLimit iterations
     while iteration <= pullLimit:
         print(f"iteration = {iteration}")
         takeScreenshot()
         cropImage()
         artName = checkArtName()
         description = checkArtDescription()
-        
+
         #TODO: logic for checking if art is requested needs to be updated for description checking
         #if artName in arts:
         #    print("Art pulled in requested arts")
         #else:
         #    print("Art pulled not in requested arts")
-        
-        #logging and updating iteration counter
+
+        #count art if logging enabled and update iteration counter
         if logging == "enable":
             countArtPulls()
         iteration += 1
-        time.sleep(1)
+        time.sleep(3)
 
-    #write log file if applicable
+    #write log file if enabled
     if logging == "enable":
         writeLogFile()
 
